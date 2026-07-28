07226adf54758d3a095042c4e5b65dffa647b7b8
nana-tarou <you@example.com>
2026-07-28 23:52:02 +0900

    Initial commit: Add basic static site with HTML/CSS/JS

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-
-console.log('site ready');
-
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('Site loaded successfully!');
+  const header = document.querySelector('header');
+  header.addEventListener('click', function() {
+    alert('Welcome to nana-tarou.github.io!');
+  });
+});
